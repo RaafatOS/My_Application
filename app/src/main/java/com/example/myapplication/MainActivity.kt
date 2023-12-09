b5350f75da504386282c14f3238b5cca8b3a71b7
khaloj <joey.khalaf@etu.emse.fr>
2023-12-09 17:04:07 +0100

    Added the API functionality to display books

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -48,7 +48,6 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback {
                 if (response.isSuccessful) {
                     val toilets:List<Toilet> = response.body()!!
                     dataset.addToilets(toilets)
-                    displayToiletsList()
                 }
             }
 
@@ -62,6 +61,7 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback {
         tab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 when (tab?.position) {
+                    0 -> displayToiletsList()
                     1 -> displayMapFragment()
                 }
             }
@@ -104,7 +104,8 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback {
     private fun displayToiletsList() {
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         fragmentTransaction.replace(
-            R.id.r_list_fragment,
+            R.id.a_const,
             ToiletListFragment.newInstance(dataset.getAllToilets()))
+        fragmentTransaction.commit()
     }
 }
