fae310ec3756c2a0db1fbe5c6e3464d379a5dc05
RaafatOS <raafatosmsna383@gmail.com>
2023-12-09 12:49:03 +0100

    ma3 list

---
+++ app/src/main/java/com/example/myapplication/toilet.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication
+
+data class toilet ( val Commune: String, val Code_Postal: String, val PointGeo: String, val Id: String
+, val Longitude: String, val OpeningHours: List<String> )
